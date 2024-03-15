52683d1aae395f710f420f64bb7de02b381d60e9
Roman Shelekhov <shelekhoff@gmail.com>
2024-03-15 22:50:43 +0400

    added additional fields to log output

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -3,12 +3,20 @@ package sso
 import (
 	"github.com/rshelekhov/sso/config"
 	"github.com/rshelekhov/sso/pkg/logger"
+	"log/slog"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
 	log := logger.SetupLogger(cfg.AppEnv)
+
+	// A field with information about the current environment
+	// will be added to each message
+	log = log.With(slog.String("env", cfg.AppEnv))
+
+	log.Info("starting application", slog.Any("config", cfg))
+	log.Debug("logger debug mode enabled")
 
 	// TODO: initialize app
 
