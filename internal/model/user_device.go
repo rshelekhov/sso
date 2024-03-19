3bb98dd026e43de05b95f35d431b9f027f4a139a
Roman Shelekhov <shelekhoff@gmail.com>
2024-03-19 23:07:02 +0400

    added UserDevice models

--- internal/model/user_device.go
+++ internal/model/user_device.go
@@ -1,1 +1,20 @@
 package model
+
+import "time"
+
+type (
+	UserDevice struct {
+		ID            string    `db:"id"`
+		UserID        string    `db:"user_id"`
+		UserAgent     string    `db:"user_agent"`
+		IP            string    `db:"ip"`
+		Detached      bool      `db:"detached"`
+		LatestLoginAt time.Time `db:"latest_login_at"`
+		DetachedAt    time.Time `db:"detached_at"`
+	}
+
+	UserDeviceRequestData struct {
+		UserAgent string
+		IP        string
+	}
+)
