47610b927d8f85c459d8c44d5d951f0e0ccb8fa1
Roman Shelekhov <shelekhoff@gmail.com>
2024-03-20 23:42:57 +0400

    refactored methods for extracting data

--- internal/grpc/auth/helpers.go
+++ internal/grpc/auth/helpers.go
@@ -2,8 +2,10 @@ package auth
 
 import (
 	"context"
+	"fmt"
 	ssov1 "github.com/rshelekhov/sso-protos/gen/go/sso"
 	"github.com/rshelekhov/sso/internal/lib/constants/le"
+	"github.com/rshelekhov/sso/internal/lib/logger"
 	"github.com/rshelekhov/sso/internal/model"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
@@ -11,26 +13,31 @@ import (
 	"strings"
 )
 
-func extractRequestID(ctx context.Context) (string, error) {
+const emptyValue = 0
+
+func extractRequestID(ctx context.Context, log logger.Interface, op string) (string, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
-		return "", le.ErrFailedToExtractMetaData
+		log.Error(fmt.Sprintf("%s: %s", op, le.ErrFailedToExtractMetaData))
+		return "", status.Error(codes.Internal, le.ErrFailedToExtractMetaData.Error())
 	}
 
 	var requestID string
 	if len(md["request-id"]) > 0 {
 		requestID = md["request-id"][0]
 	} else {
-		return "", le.ErrRequestIDIsRequired
+		log.Error(fmt.Sprintf("%s: %s", op, le.ErrRequestIDIsRequired))
+		return "", status.Error(codes.InvalidArgument, le.ErrRequestIDIsRequired.Error())
 	}
 
 	return requestID, nil
 }
 
-func extractUserDeviceData(ctx context.Context) (model.UserDeviceRequestData, error) {
+func extractUserDeviceData(ctx context.Context, log logger.Interface) (model.UserDeviceRequestData, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
-		return model.UserDeviceRequestData{}, le.ErrFailedToExtractMetaData
+		log.Error(le.ErrFailedToExtractMetaData.Error())
+		return model.UserDeviceRequestData{}, status.Error(codes.Internal, le.ErrFailedToExtractMetaData.Error())
 	}
 
 	var userAgent, ip string
@@ -38,14 +45,16 @@ func extractUserDeviceData(ctx context.Context) (model.UserDeviceRequestData, er
 	if len(md["user-agent"]) > 0 {
 		userAgent = md["user-agent"][0]
 	} else {
-		return model.UserDeviceRequestData{}, le.ErrUserAgentIsRequired
+		log.Error(le.ErrUserAgentIsRequired.Error())
+		return model.UserDeviceRequestData{}, status.Error(codes.InvalidArgument, le.ErrUserAgentIsRequired.Error())
 	}
 
 	if len(md["ip"]) > 0 {
 		ipParts := strings.Split(md["ip"][0], ":")
 		ip = ipParts[0]
 	} else {
-		return model.UserDeviceRequestData{}, le.ErrIPIsRequired
+		log.Error(le.ErrIPIsRequired.Error())
+		return model.UserDeviceRequestData{}, status.Error(codes.InvalidArgument, le.ErrIPIsRequired.Error())
 	}
 
 	return model.UserDeviceRequestData{
@@ -54,7 +63,7 @@ func extractUserDeviceData(ctx context.Context) (model.UserDeviceRequestData, er
 	}, nil
 }
 
-func validateLogin(req *ssov1.LoginRequest) error {
+func validateLogin(req *ssov1.LoginRequest, userInput *model.UserRequestData) error {
 	// TODO: add validation with validator
 	if req.GetEmail() == "" {
 		return status.Error(codes.InvalidArgument, le.ErrEmailIsRequired.Error())
@@ -68,10 +77,15 @@ func validateLogin(req *ssov1.LoginRequest) error {
 		return status.Error(codes.InvalidArgument, le.ErrAppIDIsRequired.Error())
 	}
 
+	userInput = &model.UserRequestData{
+		Email:    req.GetEmail(),
+		Password: req.GetPassword(),
+	}
+
 	return nil
 }
 
-func validateRegister(req *ssov1.RegisterRequest) error {
+func validateRegister(req *ssov1.RegisterRequest, userInput *model.UserRequestData) error {
 	// TODO: add validation with validator
 	if req.GetEmail() == "" {
 		return status.Error(codes.InvalidArgument, "email is required")
@@ -79,6 +93,11 @@ func validateRegister(req *ssov1.RegisterRequest) error {
 
 	if req.GetPassword() == "" {
 		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	userInput = &model.UserRequestData{
+		Email:    req.GetEmail(),
+		Password: req.GetPassword(),
 	}
 
 	return nil
