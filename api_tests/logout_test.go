a43041f6290d917a44717afca65fe961a229b9bb
Roman Shelekhov <shelekhoff@gmail.com>
2024-04-04 10:54:57 +0400

    moved testing requestID to a separate test

--- api_tests/logout_test.go
+++ api_tests/logout_test.go
@@ -11,77 +11,45 @@ import (
 )
 
 func TestLogoutHappyPath(t *testing.T) {
-	tests := []struct {
-		name            string
-		firstRequestID  string
-		secondRequestID string
-	}{
-		{
-			name:            "without requestID",
-			firstRequestID:  "",
-			secondRequestID: "",
+
+	ctx, st := suite.New(t)
+
+	// Generate data for request
+	email := gofakeit.Email()
+	pass := randomFakePassword()
+	userAgent := gofakeit.UserAgent()
+	ip := gofakeit.IPv4Address()
+
+	// Register user
+	respReg, err := st.AuthClient.Register(ctx, &ssov1.RegisterRequest{
+		Email:    email,
+		Password: pass,
+		AppId:    appID,
+		UserDeviceData: &ssov1.UserDeviceData{
+			UserAgent: userAgent,
+			Ip:        ip,
 		},
-		{
-			name:           "with requestID",
-			firstRequestID: "requestID #1 from the client side",
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, respReg.GetTokenData())
+
+	// Get token and place it in metadata
+	token := respReg.GetTokenData()
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, token.AccessToken)
+
+	md := metadata.Pairs(key.Token, token.AccessToken)
+
+	// Create context for Logout request
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
+	// Logout user
+	_, err = st.AuthClient.Logout(ctx, &ssov1.LogoutRequest{
+		AppId: appID,
+		UserDeviceData: &ssov1.UserDeviceData{
+			UserAgent: userAgent,
+			Ip:        ip,
 		},
-	}
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			ctx, st := suite.New(t)
-
-			// Create metadata
-			md := metadata.Pairs()
-			if tc.firstRequestID != "" {
-				md.Append(key.RequestID, tc.firstRequestID)
-			}
-
-			// Create context with metadata
-			ctx = metadata.NewOutgoingContext(ctx, md)
-
-			// Generate data for request
-			email := gofakeit.Email()
-			pass := randomFakePassword()
-			userAgent := gofakeit.UserAgent()
-			ip := gofakeit.IPv4Address()
-
-			// Register user
-			respReg, err := st.AuthClient.Register(ctx, &ssov1.RegisterRequest{
-				Email:    email,
-				Password: pass,
-				AppId:    appID,
-				UserDeviceData: &ssov1.UserDeviceData{
-					UserAgent: userAgent,
-					Ip:        ip,
-				},
-			})
-			require.NoError(t, err)
-			require.NotEmpty(t, respReg.GetTokenData())
-
-			token := respReg.GetTokenData()
-			require.NotEmpty(t, token)
-			require.NotEmpty(t, token.AccessToken)
-
-			md = metadata.Pairs(key.Token, token.AccessToken)
-
-			if tc.secondRequestID != "" {
-				md.Append(key.RequestID, tc.secondRequestID)
-			}
-
-			// Create context for Logout request
-			ctx = metadata.NewOutgoingContext(ctx, md)
-
-			// Logout user
-			_, err = st.AuthClient.Logout(ctx, &ssov1.LogoutRequest{
-				AppId: appID,
-				UserDeviceData: &ssov1.UserDeviceData{
-					UserAgent: userAgent,
-					Ip:        ip,
-				},
-			})
-
-			require.NoError(t, err)
-		})
-	}
+	})
+	require.NoError(t, err)
 }
