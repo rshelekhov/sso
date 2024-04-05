d4f4e02f1860fa33a15fa5a01bddd9778a3d3abe
Roman Shelekhov <shelekhoff@gmail.com>
2024-04-06 00:04:14 +0400

    added tests

--- internal/lib/jwt/bcrypt_hash.go
+++ internal/lib/jwt/bcrypt_hash.go
@@ -4,13 +4,18 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"errors"
-
+	"fmt"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func PasswordHashBcrypt(password string, cost int, salt []byte) (string, error) {
-	if cost <= 0 {
-		cost = bcrypt.DefaultCost
+	const method = "jwt.PasswordHashBcrypt"
+
+	if password == "" {
+		return "", fmt.Errorf("%s: password is empty", method)
+	}
+	if len(salt) == 0 {
+		return "", fmt.Errorf("%s: salt is empty", method)
 	}
 
 	passwordHmac := hmac.New(sha256.New, salt)
@@ -28,6 +33,16 @@ func PasswordHashBcrypt(password string, cost int, salt []byte) (string, error) 
 }
 
 func PasswordMatch(hash, password string, salt []byte) (bool, error) {
+	const method = "jwt.PasswordMatch"
+
+	if hash == "" {
+		return false, fmt.Errorf("%s: hash is empty", method)
+	} else if password == "" {
+		return false, fmt.Errorf("%s: password is empty", method)
+	} else if len(salt) == 0 {
+		return false, fmt.Errorf("%s: salt is empty", method)
+	}
+
 	passwordHmac := hmac.New(sha256.New, salt)
 	_, err := passwordHmac.Write([]byte(password))
 	if err != nil {
@@ -35,8 +50,11 @@ func PasswordMatch(hash, password string, salt []byte) (bool, error) {
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(hash), passwordHmac.Sum(nil))
-	if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
-		return false, nil
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return false, nil
+		}
+		return false, err
 	}
 
 	return true, nil
