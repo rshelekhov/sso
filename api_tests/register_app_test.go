e9ad84b2362b745d8c865e3571984b566b2acf1b
Roman Shelekhov <shelekhoff@gmail.com>
2024-07-10 14:29:04 +0400

    added tests for registering app method

--- api_tests/register_app_test.go
+++ api_tests/register_app_test.go
@@ -1,1 +1,65 @@
 package api_tests
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	ssov1 "github.com/rshelekhov/sso-protos/gen/go/sso"
+	"github.com/rshelekhov/sso/api_tests/suite"
+	"github.com/rshelekhov/sso/internal/lib/constant/le"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestRegisterApp_HappyPath(t *testing.T) {
+	ctx, st := suite.New(t)
+
+	appName := gofakeit.Word()
+
+	// Register app
+	_, err := st.AuthClient.RegisterApp(ctx, &ssov1.RegisterAppRequest{
+		AppName: appName,
+	})
+	require.NoError(t, err)
+}
+
+func TestRegisterApp_EmptyAppName(t *testing.T) {
+	ctx, st := suite.New(t)
+
+	// Register app
+	_, err := st.AuthClient.RegisterApp(ctx, &ssov1.RegisterAppRequest{
+		AppName: "",
+	})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), le.ErrAppNameIsRequired.Error())
+}
+
+func TestRegisterApp_AppNameWithSpaces(t *testing.T) {
+	ctx, st := suite.New(t)
+
+	appName := gofakeit.Word() + " " + gofakeit.Word()
+
+	// Register app
+	_, err := st.AuthClient.RegisterApp(ctx, &ssov1.RegisterAppRequest{
+		AppName: appName,
+	})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), le.ErrAppNameCannotContainSpaces.Error())
+}
+
+func TestRegisterApp_AppAlreadyExists(t *testing.T) {
+	ctx, st := suite.New(t)
+
+	appName := gofakeit.Word()
+
+	// Register first app
+	_, err := st.AuthClient.RegisterApp(ctx, &ssov1.RegisterAppRequest{
+		AppName: appName,
+	})
+	require.NoError(t, err)
+
+	// Register second app
+	_, err = st.AuthClient.RegisterApp(ctx, &ssov1.RegisterAppRequest{
+		AppName: appName,
+	})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), le.ErrAppAlreadyExists.Error())
+}
