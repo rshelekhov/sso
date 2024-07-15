8a8f77ece899fd9b30b56b679c1384421bbf3e84
Roman Shelekhov <shelekhoff@gmail.com>
2024-07-15 22:41:24 +0400

    added tests for RegisterApp method

--- api_tests/register_app_test.go
+++ api_tests/register_app_test.go
@@ -1,11 +1,18 @@
 package api_tests
 
 import (
+	"errors"
 	"github.com/brianvoe/gofakeit/v6"
 	ssov1 "github.com/rshelekhov/sso-protos/gen/go/sso"
+	"github.com/rshelekhov/sso/api_tests/mocks"
 	"github.com/rshelekhov/sso/api_tests/suite"
+	"github.com/rshelekhov/sso/internal/config"
 	"github.com/rshelekhov/sso/internal/lib/constant/le"
+	"github.com/rshelekhov/sso/internal/usecase"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"log/slog"
+	"os"
 	"testing"
 )
 
@@ -62,4 +69,84 @@ func TestRegisterApp_AppAlreadyExists(t *testing.T) {
 	})
 	require.Error(t, err)
 	require.Contains(t, err.Error(), le.ErrAppAlreadyExists.Error())
+}
+
+// TestRegisterApp_GeneratePEMKeyPairError tests the scenario where the GeneratePEMKeyPair function
+// returns an error and then app is deleted
+func TestRegisterApp_GeneratePEMKeyPairError(t *testing.T) {
+	ctx, _ := suite.New(t)
+
+	// Create mocks
+	mockTS := mocks.NewMockTokenService()
+	mockStorage := new(mocks.MockAppStorage)
+
+	// Set up the mock to return an error when GeneratePEMKeyPair is called
+	mockTS.On("GeneratePEMKeyPair", mock.Anything).Return(errors.New("failed to generate PEM key pair"))
+	mockStorage.On("RegisterApp", mock.Anything, mock.Anything).Return(nil)
+	mockStorage.On("DeleteApp", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+
+	cfg := &config.ServerSettings{
+		DefaultHashBcrypt: config.HashBcryptConfig{
+			Salt: "salt",
+		},
+	}
+	log := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+
+	// Create an instance of AppUsecase with the mocks
+	appUsecase := usecase.NewAppUsecase(cfg, log, mockStorage, mockTS)
+
+	appName := gofakeit.Word()
+
+	// Register app and expect an error
+	err := appUsecase.RegisterApp(ctx, appName)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "internal server error")
+
+	// Verify that GeneratePEMKeyPair was called
+	mockTS.AssertCalled(t, "GeneratePEMKeyPair", mock.Anything)
+
+	// Verify that DeleteApp was called after the error
+	mockStorage.AssertCalled(t, "DeleteApp", mock.Anything, mock.Anything, mock.Anything)
+
+	// Assert that no other methods on mockTS were called
+	mockTS.AssertExpectations(t)
+}
+
+func TestRegisterApp_GeneratePEMKeyPairError_DeleteAppError(t *testing.T) {
+	ctx, _ := suite.New(t)
+
+	// Create mocks
+	mockTS := mocks.NewMockTokenService()
+	mockStorage := new(mocks.MockAppStorage)
+
+	// Set up the mock to return an error when GeneratePEMKeyPair is called
+	mockTS.On("GeneratePEMKeyPair", mock.Anything).Return(errors.New("failed to generate PEM key pair"))
+	mockStorage.On("RegisterApp", mock.Anything, mock.Anything).Return(nil)
+	mockStorage.On("DeleteApp", mock.Anything, mock.Anything, mock.Anything).Return(errors.New("failed to delete app"))
+
+	cfg := &config.ServerSettings{
+		DefaultHashBcrypt: config.HashBcryptConfig{
+			Salt: "salt",
+		},
+	}
+	log := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+
+	// Create an instance of AppUsecase with the mocks
+	appUsecase := usecase.NewAppUsecase(cfg, log, mockStorage, mockTS)
+
+	appName := gofakeit.Word()
+
+	// Register app and expect an error
+	err := appUsecase.RegisterApp(ctx, appName)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "internal server error")
+
+	// Verify that GeneratePEMKeyPair was called
+	mockTS.AssertCalled(t, "GeneratePEMKeyPair", mock.Anything)
+
+	// Verify that DeleteApp was called after the error
+	mockStorage.AssertCalled(t, "DeleteApp", mock.Anything, mock.Anything, mock.Anything)
+
+	// Assert that no other methods on mockTS were called
+	mockTS.AssertExpectations(t)
 }
