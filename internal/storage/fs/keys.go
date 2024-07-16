a2f348846c54794afb39c9e8400a7bc6844a77e4
Roman Shelekhov <shelekhoff@gmail.com>
2024-07-16 23:52:40 +0400

    added local storage

--- internal/storage/fs/keys.go
+++ internal/storage/fs/keys.go
@@ -1,13 +1,45 @@
 package fs
 
-import "github.com/rshelekhov/sso/internal/config"
+import (
+	"fmt"
+	"github.com/rshelekhov/sso/internal/config"
+	"os"
+)
 
 type KeyStorage struct {
 	KeysPath string
 }
 
-func NewKeyStorage(settings config.KeyStorageLocal) *KeyStorage {
+func NewKeyStorage(settings config.KeyStorageLocal) (*KeyStorage, error) {
 	return &KeyStorage{
 		KeysPath: settings.Path,
+	}, nil
+}
+
+const privateKeyFilePathFormat = "%s/app_%s_private.pem"
+
+func (s *KeyStorage) SavePrivateKey(appID string, privateKeyPEM []byte) error {
+	// Ensure the keysPath directory exists
+	if err := os.MkdirAll(s.KeysPath, os.ModePerm); err != nil {
+		return fmt.Errorf("failed to create keys path: %w", err)
 	}
+
+	privateKeyFilePath := fmt.Sprintf(privateKeyFilePathFormat, s.KeysPath, appID)
+
+	if err := os.WriteFile(privateKeyFilePath, privateKeyPEM, 0600); err != nil {
+		return fmt.Errorf("failed to save private key to file: %w", err)
+	}
+
+	return nil
+}
+
+func (s *KeyStorage) GetPrivateKey(appID string) ([]byte, error) {
+	privateKeyFilePath := fmt.Sprintf(privateKeyFilePathFormat, s.KeysPath, appID)
+
+	privateKeyBytes, err := os.ReadFile(privateKeyFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read private key: %w", err)
+	}
+
+	return privateKeyBytes, nil
 }
