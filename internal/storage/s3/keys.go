8ac39a21a1a3b9963cdad6292f03d6e16280c2cd
Roman Shelekhov <shelekhoff@gmail.com>
2024-07-19 00:17:39 +0400

    implement S3 key storage

--- internal/storage/s3/keys.go
+++ internal/storage/s3/keys.go
@@ -1,9 +1,71 @@
 package s3
 
-import "github.com/aws/aws-sdk-go/service/s3"
+import (
+	"bytes"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/rshelekhov/sso/internal/config"
+	"io"
+)
 
 type KeyStorage struct {
-	Client   *s3.S3
-	Bucket   string
-	KeysPath string
+	Client         *s3.S3
+	Bucket         string
+	PrivateKeyPath string
+}
+
+func NewKeyStorage(settings config.KeyStorageS3) (*KeyStorage, error) {
+	s3Client, err := NewS3Client(
+		settings.Region,
+		settings.Endpoint,
+		settings.AccessKey,
+		settings.SecretKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &KeyStorage{
+		Client:         s3Client,
+		Bucket:         settings.Bucket,
+		PrivateKeyPath: settings.PrivateKeyPath,
+	}, nil
+}
+
+const privateKeyFilePathFormat = "%s/app_%s_private.pem"
+
+func (s *KeyStorage) SavePrivateKey(appID string, privateKeyPEM []byte) error {
+	privateKeyFilePath := fmt.Sprintf(privateKeyFilePathFormat, s.PrivateKeyPath, appID)
+
+	_, err := s.Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s.Bucket),
+		Key:    aws.String(privateKeyFilePath),
+		Body:   bytes.NewReader(privateKeyPEM),
+		ACL:    aws.String("private"),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to save private key to S3: %w", err)
+	}
+
+	return nil
+}
+
+func (s *KeyStorage) GetPrivateKey(appID string) ([]byte, error) {
+	privateKeyFilePath := fmt.Sprintf(privateKeyFilePathFormat, s.PrivateKeyPath, appID)
+
+	result, err := s.Client.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(s.Bucket),
+		Key:    aws.String(privateKeyFilePath),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get private key from S3: %w", err)
+	}
+	defer result.Body.Close()
+
+	privateKeyBytes, err := io.ReadAll(result.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read private key data: %w", err)
+	}
+
+	return privateKeyBytes, nil
 }
