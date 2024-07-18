6ef4ca8b10b43dd044d76b1531d55ce02d5c7322
Roman Shelekhov <shelekhoff@gmail.com>
2024-07-19 00:16:46 +0400

    refactored tests for token service

--- internal/lib/jwt/jwtoken/mocks/mock_key_storage.go
+++ internal/lib/jwt/jwtoken/mocks/mock_key_storage.go
@@ -1,1 +1,21 @@
 package mocks
+
+import (
+	"github.com/rshelekhov/sso/internal/port"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockKeyStorage struct {
+	port.KeyStorage
+	mock.Mock
+}
+
+func (m *MockKeyStorage) SavePrivateKey(appID string, privateKeyPEM []byte) error {
+	args := m.Called(appID, privateKeyPEM)
+	return args.Error(0)
+}
+
+func (m *MockKeyStorage) GetPrivateKey(appID string) ([]byte, error) {
+	args := m.Called(appID)
+	return args.Get(0).([]byte), args.Error(1)
+}
