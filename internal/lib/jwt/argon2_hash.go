4f763c7dfd02c6c199db2b60bb4bf18d0eb24ba3
Roman Shelekhov <shelekhoff@gmail.com>
2024-08-20 23:44:56 +0400

    implement argon2 hash algorithm

--- internal/lib/jwt/argon2_hash.go
+++ internal/lib/jwt/argon2_hash.go
@@ -1,44 +1,116 @@
-package jwtoken
+package jwt
 
 import (
+	"crypto/subtle"
 	"encoding/base64"
 	"fmt"
+	"github.com/rshelekhov/sso/internal/config/settings"
 	"golang.org/x/crypto/argon2"
+	"strings"
 )
 
-// Parameters for Argon2
-var (
-	iterations  uint32 = 3
-	memory      uint32 = 64 * 1024
-	parallelism uint8  = 2
-	keyLength   uint32 = 32
-)
+func PasswordHashArgon2(password string, p settings.PasswordHashArgon2Params, salt, pepper []byte) string {
+	saltAndPepper := combineSaltAndPepper(salt, pepper)
 
-func PasswordHashArgon2(password string, salt []byte) (string, error) {
-	const method = "jwt.PasswordHashArgon2"
+	hash := argon2.IDKey(
+		[]byte(password),
+		saltAndPepper,
+		p.Time,
+		p.Memory,
+		p.Parallelism,
+		p.KeyLength,
+	)
 
-	if password == "" {
-		return "", fmt.Errorf("%s: password is empty", method)
-	}
-	if len(salt) == 0 {
-		return "", fmt.Errorf("%s: salt is empty", method)
-	}
+	saltBase64 := base64.RawStdEncoding.EncodeToString(salt)
+	hashBase64 := base64.RawStdEncoding.EncodeToString(hash)
 
-	hash := argon2.IDKey([]byte(password), salt, iterations, memory, parallelism, keyLength)
+	// More details about this formatting can be found here:
+	// https://github.com/P-H-C/phc-winner-argon2#command-line-utility
+	hashStr := fmt.Sprintf(
+		"$%s$v=%d$m=%d,t=%d,p=%d$%s$%s",
+		string(settings.PasswordHashArgon2),
+		argon2.Version,
+		p.Memory,
+		p.Time,
+		p.Parallelism,
+		saltBase64,
+		hashBase64,
+	)
 
-	hashBase64 := base64.StdEncoding.EncodeToString(hash)
-	return hashBase64, nil
+	return hashStr
 }
 
-func PasswordMatchArgon2(hash, password string, salt []byte) (bool, error) {
+func PasswordMatchArgon2(hashStr, password string, pepper []byte) (bool, error) {
 	const method = "jwt.PasswordMatchArgon2"
 
-	if hash == "" {
-		return false, fmt.Errorf("%s: hash is empty", method)
-	} else if password == "" {
-		return false, fmt.Errorf("%s: password is empty", method)
-	} else if len(salt) == 0 {
-		return false, fmt.Errorf("%s: salt is empty", method)
+	p := settings.PasswordHashArgon2Params{}
+	parts := strings.Split(hashStr, "$")
+	if len(parts) != 6 {
+		return false, fmt.Errorf("%s: invalid argon hash string: %s", method, hashStr)
 	}
 
+	var version int
+	_, err := fmt.Sscanf(parts[2], "v=%d", &version)
+	if err != nil {
+		return false, err
+	}
+	if version != argon2.Version {
+		return false, fmt.Errorf("%s: unsupported Argon2 version: %d", method, version)
+	}
+
+	_, err = fmt.Sscanf(parts[3], "m=%d,t=%d,p=%d", &p.Memory, &p.Time, &p.Parallelism)
+	if err != nil {
+		return false, err
+	}
+
+	saltBase64 := parts[4]
+	hashBase64 := parts[5]
+
+	salt, err := base64.RawStdEncoding.Strict().DecodeString(saltBase64)
+	if err != nil {
+		return false, err
+	}
+
+	hash, err := base64.RawStdEncoding.Strict().DecodeString(hashBase64)
+	if err != nil {
+		return false, err
+	}
+
+	p.KeyLength = uint32(len(hash))
+
+	saltAndPepper := combineSaltAndPepper(salt, pepper)
+
+	computedHash := argon2.IDKey(
+		[]byte(password),
+		saltAndPepper,
+		p.Time,
+		p.Memory,
+		p.Parallelism,
+		p.KeyLength,
+	)
+
+	// Compare the stored hash with the computed hash using ConstantTimeCompare.
+	// This function performs the comparison in constant time, which is critical
+	// to prevent timing attacks. Timing attacks exploit the time differences in
+	// string comparisons to infer the correct password. By using a constant time
+	// comparison, we ensure that the time taken to compare the hashes is the same
+	// regardless of how much of the input matches, making it more secure.
+	if subtle.ConstantTimeCompare(hash, computedHash) == 1 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// combineSaltAndPepper merges two byte slices, salt and pepper, into a single byte slice.
+// It first pre-allocates a new slice with an exact combined capacity of both pepper and pepper.
+// This is done using the length of pepper and pepper, which improves performance by avoiding
+// multiple allocations as the slice grows dynamically during the append operations.
+// By pre-allocating the necessary capacity, we minimize memory fragmentation and ensure
+// that the final slice contains pepper followed by pepper in a single continuous block.
+func combineSaltAndPepper(salt, pepper []byte) []byte {
+	combined := make([]byte, 0, len(salt)+len(pepper))
+	combined = append(combined, salt...)
+	combined = append(combined, pepper...)
+	return combined
 }
--- internal/lib/jwt/argon2_hash_test.go
+++ internal/lib/jwt/argon2_hash_test.go
@@ -1,1 +1,66 @@
 package jwt
+
+import (
+	"encoding/base64"
+	"github.com/rshelekhov/sso/internal/config/settings"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestPasswordHashArgon2_HappyPath(t *testing.T) {
+	pass := "test-password"
+	params := settings.DefaultPasswordHashArgon2Params
+	salt := []byte("salt")
+	pepper := []byte("pepper")
+
+	hash := PasswordHashArgon2(pass, params, salt, pepper)
+	require.NotEmpty(t, hash)
+
+	assert.Contains(t, hash, "$argon2$")
+	assert.Contains(t, hash, "$v=")
+	assert.Contains(t, hash, "$m=")
+	assert.Contains(t, hash, ",t=")
+	assert.Contains(t, hash, ",p=")
+	assert.Contains(t, hash, base64.RawStdEncoding.EncodeToString(salt))
+}
+
+func TestPasswordMatchArgon2_HappyPath(t *testing.T) {
+	pass := "test-password"
+	params := settings.DefaultPasswordHashArgon2Params
+	salt := []byte("salt")
+	pepper := []byte("pepper")
+
+	hash := PasswordHashArgon2(pass, params, salt, pepper)
+	require.NotEmpty(t, hash)
+
+	matched, err := PasswordMatchArgon2(hash, pass, pepper)
+	require.NoError(t, err)
+	require.True(t, matched)
+}
+
+func TestPasswordMatchArgon2_IncorrectPassword(t *testing.T) {
+	params := settings.DefaultPasswordHashArgon2Params
+	salt := []byte("salt")
+	pepper := []byte("pepper")
+
+	hash := PasswordHashArgon2("test-password", params, salt, pepper)
+	require.NotEmpty(t, hash)
+
+	matched, _ := PasswordMatchArgon2(hash, "incorrect-password", pepper)
+	require.False(t, matched)
+
+}
+
+func TestPasswordMatchArgon2_EmptyPassword(t *testing.T) {
+	params := settings.DefaultPasswordHashArgon2Params
+	salt := []byte("salt")
+	pepper := []byte("pepper")
+
+	hash := PasswordHashArgon2("test-password", params, salt, pepper)
+	require.NotEmpty(t, hash)
+
+	matched, _ := PasswordMatchArgon2(hash, "", pepper)
+	require.False(t, matched)
+
+}
