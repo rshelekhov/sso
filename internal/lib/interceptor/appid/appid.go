eb018fa886b89427decfa0a270fa132d3f86b19e
Roman Shelekhov <shelekhoff@gmail.com>
2025-01-23 00:18:19 +0400

    add interceptors

--- internal/lib/interceptor/appid/appid.go
+++ internal/lib/interceptor/appid/appid.go
@@ -1,1 +1,24 @@
-package appID
+package appid
+
+import (
+	"context"
+	"github.com/rshelekhov/sso/internal/config/grpcmethods"
+	"google.golang.org/grpc"
+)
+
+func UnaryServerInterceptor(cfg *grpcmethods.Methods, appIDInterceptor grpc.UnaryServerInterceptor) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		// Check appID and place it to context
+		if cfg.IsAppIDRequired(info.FullMethod) {
+			return appIDInterceptor(ctx, req, info, handler)
+		}
+
+		// Continue without appID
+		return handler(ctx, req)
+	}
+}
--- internal/lib/interceptor/auth/auth.go
+++ internal/lib/interceptor/auth/auth.go
@@ -2,23 +2,24 @@ package auth
 
 import (
 	"context"
+
 	"github.com/rshelekhov/sso/internal/config/grpcmethods"
 	"google.golang.org/grpc"
 )
 
-func UnaryServerInterceptor(cfg *grpcmethods.Methods, jwtMiddleware grpc.UnaryServerInterceptor) grpc.UnaryServerInterceptor {
+func UnaryServerInterceptor(cfg *grpcmethods.Methods, jwtInterceptor grpc.UnaryServerInterceptor) grpc.UnaryServerInterceptor {
 	return func(
 		ctx context.Context,
 		req interface{},
 		info *grpc.UnaryServerInfo,
 		handler grpc.UnaryHandler,
 	) (interface{}, error) {
-		// Skip auth for public methods
-		if cfg.IsPublic(info.FullMethod) {
-			return handler(ctx, req)
+		// Check token and place it to context
+		if cfg.IsTokenRequired(info.FullMethod) {
+			return jwtInterceptor(ctx, req, info, handler)
 		}
 
-		// Check token and place it to context
-		return jwtMiddleware(ctx, req, info, handler)
+		// Continue without token
+		return handler(ctx, req)
 	}
 }
