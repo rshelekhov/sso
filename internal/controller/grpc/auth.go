68890ec0b75ed025acd98385047b823f76da7e65
Roman Shelekhov <shelekhoff@gmail.com>
2025-01-28 00:25:16 +0400

    update error handling

--- internal/controller/grpc/auth.go
+++ internal/controller/grpc/auth.go
@@ -2,70 +2,73 @@ package grpc
 
 import (
 	"context"
-	ssov1 "github.com/rshelekhov/sso-protos/gen/go/sso"
-	"github.com/rshelekhov/sso/src/lib/e"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"fmt"
 	"log/slog"
+
+	"github.com/rshelekhov/sso/internal/controller"
+
+	ssov1 "github.com/rshelekhov/sso-protos/gen/go/sso"
+	"github.com/rshelekhov/sso/internal/domain"
+	"github.com/rshelekhov/sso/internal/lib/e"
 )
 
-func (c *controller) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
+func (c *gRPCController) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
 	const method = "controller.gRPC.Login"
 
 	log := c.log.With(slog.String("method", method))
 
 	reqID, err := c.getRequestID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetRequestID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetRequestID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetRequestID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetRequestID, err))
 	}
 
 	log = log.With(slog.String("requestID", reqID))
 
 	if err = validateLoginRequest(req); err != nil {
-		e.HandleError(ctx, log, ErrValidationError, err)
-		return nil, status.Errorf(codes.InvalidArgument, "%v: %v", ErrValidationError, err)
+		e.LogError(ctx, log, controller.ErrValidationError, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrValidationError, err))
 	}
 
 	appID, err := c.getAndValidateAppID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetAndValidateAppID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetAndValidateAppID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetAndValidateAppID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetAndValidateAppID, err))
 	}
 
 	userData := fromLoginRequest(req)
 
 	tokenData, err := c.authUsecase.Login(ctx, appID, userData)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToLoginUser, err)
+		e.LogError(ctx, log, controller.ErrFailedToLoginUser, err)
 		return nil, mapErrorToGRPCStatus(err)
 	}
 
 	return toLoginResponse(tokenData), nil
 }
 
-func (c *controller) RegisterUser(ctx context.Context, req *ssov1.RegisterUserRequest) (*ssov1.RegisterUserResponse, error) {
+func (c *gRPCController) RegisterUser(ctx context.Context, req *ssov1.RegisterUserRequest) (*ssov1.RegisterUserResponse, error) {
 	const method = "controller.gRPC.RegisterUser"
 
 	log := c.log.With(slog.String("method", method))
 
 	reqID, err := c.getRequestID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetRequestID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetRequestID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetRequestID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetRequestID, err))
 	}
 
 	log = log.With(slog.String("requestID", reqID))
 
 	if err = validateRegisterUserRequest(req); err != nil {
-		e.HandleError(ctx, log, ErrValidationError, err)
-		return nil, status.Errorf(codes.InvalidArgument, "%v: %v", ErrValidationError, err)
+		e.LogError(ctx, log, controller.ErrValidationError, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrValidationError, err))
 	}
 
 	appID, err := c.getAndValidateAppID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetAndValidateAppID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetAndValidateAppID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetAndValidateAppID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetAndValidateAppID, err))
 	}
 
 	userData := fromRegisterUserRequest(req)
@@ -73,64 +76,68 @@ func (c *controller) RegisterUser(ctx context.Context, req *ssov1.RegisterUserRe
 
 	tokenData, err := c.authUsecase.RegisterUser(ctx, appID, userData, endpoint)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToRegisterUser, err)
+		e.LogError(ctx, log, controller.ErrFailedToRegisterUser, err)
 		return nil, mapErrorToGRPCStatus(err)
 	}
 
 	return toRegisterUserResponse(tokenData), nil
 }
 
-func (c *controller) VerifyEmail(ctx context.Context, req *ssov1.VerifyEmailRequest) (*ssov1.VerifyEmailResponse, error) {
+func (c *gRPCController) VerifyEmail(ctx context.Context, req *ssov1.VerifyEmailRequest) (*ssov1.VerifyEmailResponse, error) {
 	const method = "controller.gRPC.VerifyEmail"
 
 	log := c.log.With(slog.String("method", method))
 
 	reqID, err := c.getRequestID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetRequestID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetRequestID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetRequestID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetRequestID, err))
 	}
 
 	log = log.With(slog.String("requestID", reqID))
 
 	if err = validateVerifyEmailRequest(req); err != nil {
-		e.HandleError(ctx, log, ErrValidationError, err)
-		return nil, status.Errorf(codes.InvalidArgument, "%v: %v", ErrValidationError, err)
+		e.LogError(ctx, log, controller.ErrValidationError, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrValidationError, err))
 	}
 
 	verificationToken := req.GetToken()
 
-	err = c.authUsecase.VerifyEmail(ctx, verificationToken)
+	res, err := c.authUsecase.VerifyEmail(ctx, verificationToken)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToVerifyEmail, err)
+		e.LogError(ctx, log, controller.ErrFailedToVerifyEmail, err)
 		return nil, mapErrorToGRPCStatus(err)
+	}
+
+	if res.TokenExpired {
+		return nil, mapErrorToGRPCStatus(domain.ErrTokenExpiredWithEmailResent)
 	}
 
 	return &ssov1.VerifyEmailResponse{}, nil
 }
 
-func (c *controller) ResetPassword(ctx context.Context, req *ssov1.ResetPasswordRequest) (*ssov1.ResetPasswordResponse, error) {
+func (c *gRPCController) ResetPassword(ctx context.Context, req *ssov1.ResetPasswordRequest) (*ssov1.ResetPasswordResponse, error) {
 	const method = "controller.gRPC.ResetPassword"
 
 	log := c.log.With(slog.String("method", method))
 
 	reqID, err := c.getRequestID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetRequestID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetRequestID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetRequestID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetRequestID, err))
 	}
 
 	log = log.With(slog.String("requestID", reqID))
 
 	if err = validateResetPasswordRequest(req); err != nil {
-		e.HandleError(ctx, log, ErrValidationError, err)
-		return nil, status.Errorf(codes.InvalidArgument, "%v: %v", ErrValidationError, err)
+		e.LogError(ctx, log, controller.ErrValidationError, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrValidationError, err))
 	}
 
 	appID, err := c.getAndValidateAppID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetAndValidateAppID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetAndValidateAppID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetAndValidateAppID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetAndValidateAppID, err))
 	}
 
 	reqData := fromResetPasswordRequest(req)
@@ -138,139 +145,143 @@ func (c *controller) ResetPassword(ctx context.Context, req *ssov1.ResetPassword
 
 	err = c.authUsecase.ResetPassword(ctx, appID, reqData, endpoint)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToResetPassword, err)
+		e.LogError(ctx, log, controller.ErrFailedToResetPassword, err)
 		return nil, mapErrorToGRPCStatus(err)
 	}
 
 	return &ssov1.ResetPasswordResponse{}, nil
 }
 
-func (c *controller) ChangePassword(ctx context.Context, req *ssov1.ChangePasswordRequest) (*ssov1.ChangePasswordResponse, error) {
+func (c *gRPCController) ChangePassword(ctx context.Context, req *ssov1.ChangePasswordRequest) (*ssov1.ChangePasswordResponse, error) {
 	const method = "controller.gRPC.ChangePassword"
 
 	log := c.log.With(slog.String("method", method))
 
 	reqID, err := c.getRequestID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetRequestID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetRequestID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetRequestID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetRequestID, err))
 	}
 
 	log = log.With(slog.String("requestID", reqID))
 
 	if err = validateChangePasswordRequest(req); err != nil {
-		e.HandleError(ctx, log, ErrValidationError, err)
-		return nil, status.Errorf(codes.InvalidArgument, "%v: %v", ErrValidationError, err)
+		e.LogError(ctx, log, controller.ErrValidationError, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrValidationError, err))
 	}
 
 	appID, err := c.getAndValidateAppID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetAndValidateAppID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetAndValidateAppID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetAndValidateAppID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetAndValidateAppID, err))
 	}
 
 	reqData := fromChangePasswordRequest(req)
 
-	err = c.authUsecase.ChangePassword(ctx, appID, reqData)
+	res, err := c.authUsecase.ChangePassword(ctx, appID, reqData)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToChangePassword, err)
+		e.LogError(ctx, log, controller.ErrFailedToChangePassword, err)
 		return nil, mapErrorToGRPCStatus(err)
+	}
+
+	if res.TokenExpired {
+		return nil, mapErrorToGRPCStatus(domain.ErrTokenExpiredWithEmailResent)
 	}
 
 	return &ssov1.ChangePasswordResponse{}, nil
 }
 
-func (c *controller) Logout(ctx context.Context, req *ssov1.LogoutRequest) (*ssov1.LogoutResponse, error) {
+func (c *gRPCController) Logout(ctx context.Context, req *ssov1.LogoutRequest) (*ssov1.LogoutResponse, error) {
 	const method = "controller.gRPC.Logout"
 
 	log := c.log.With(slog.String("method", method))
 
 	reqID, err := c.getRequestID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetRequestID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetRequestID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetRequestID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetRequestID, err))
 	}
 
 	log = log.With(slog.String("requestID", reqID))
 
 	if err = validateLogoutRequest(req); err != nil {
-		e.HandleError(ctx, log, ErrValidationError, err)
-		return nil, status.Errorf(codes.InvalidArgument, "%v: %v", ErrValidationError, err)
+		e.LogError(ctx, log, controller.ErrValidationError, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrValidationError, err))
 	}
 
 	appID, err := c.getAndValidateAppID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetAndValidateAppID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetAndValidateAppID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetAndValidateAppID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetAndValidateAppID, err))
 	}
 
 	reqData := fromLogoutRequest(req)
 
 	err = c.authUsecase.LogoutUser(ctx, appID, reqData)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToLogoutUser, err)
+		e.LogError(ctx, log, controller.ErrFailedToLogoutUser, err)
 		return nil, mapErrorToGRPCStatus(err)
 	}
 
 	return &ssov1.LogoutResponse{}, nil
 }
 
-func (c *controller) Refresh(ctx context.Context, req *ssov1.RefreshRequest) (*ssov1.RefreshResponse, error) {
+func (c *gRPCController) Refresh(ctx context.Context, req *ssov1.RefreshRequest) (*ssov1.RefreshResponse, error) {
 	const method = "controller.gRPC.Refresh"
 
 	log := c.log.With(slog.String("method", method))
 
 	reqID, err := c.getRequestID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetRequestID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetRequestID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetRequestID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetRequestID, err))
 	}
 
 	log = log.With(slog.String("requestID", reqID))
 
 	if err = validateRefreshRequest(req); err != nil {
-		e.HandleError(ctx, log, ErrValidationError, err)
-		return nil, status.Errorf(codes.InvalidArgument, "%v: %v", ErrValidationError, err)
+		e.LogError(ctx, log, controller.ErrValidationError, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrValidationError, err))
 	}
 
 	appID, err := c.getAndValidateAppID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetAndValidateAppID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetAndValidateAppID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetAndValidateAppID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetAndValidateAppID, err))
 	}
 
 	reqData := fromRefreshRequest(req)
 
 	tokenData, err := c.authUsecase.RefreshTokens(ctx, appID, reqData)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToRefreshTokens, err)
+		e.LogError(ctx, log, controller.ErrFailedToRefreshTokens, err)
 		return nil, mapErrorToGRPCStatus(err)
 	}
 	return toRefreshResponse(tokenData), nil
 }
 
-func (c *controller) GetJWKS(ctx context.Context, req *ssov1.GetJWKSRequest) (*ssov1.GetJWKSResponse, error) {
+func (c *gRPCController) GetJWKS(ctx context.Context, req *ssov1.GetJWKSRequest) (*ssov1.GetJWKSResponse, error) {
 	const method = "controller.gRPC.GetJWKS"
 
 	log := c.log.With(slog.String("method", method))
 
 	reqID, err := c.getRequestID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetRequestID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetRequestID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetRequestID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetRequestID, err))
 	}
 
 	log = log.With(slog.String("requestID", reqID))
 
 	appID, err := c.getAndValidateAppID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetAndValidateAppID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetAndValidateAppID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetAndValidateAppID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetAndValidateAppID, err))
 	}
 
 	jwks, err := c.authUsecase.GetJWKS(ctx, appID)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetJWKS, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetJWKS, err)
 		return nil, mapErrorToGRPCStatus(err)
 	}
 
--- internal/controller/grpc/user.go
+++ internal/controller/grpc/user.go
@@ -2,35 +2,40 @@ package grpc
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/rshelekhov/sso/internal/controller"
+
 	ssov1 "github.com/rshelekhov/sso-protos/gen/go/sso"
-	"github.com/rshelekhov/sso/src/lib/e"
+	"github.com/rshelekhov/sso/internal/lib/e"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"log/slog"
 )
 
-func (c *controller) GetUser(ctx context.Context, req *ssov1.GetUserRequest) (*ssov1.GetUserResponse, error) {
+func (c *gRPCController) GetUser(ctx context.Context, req *ssov1.GetUserRequest) (*ssov1.GetUserResponse, error) {
 	const method = "controller.gRPC.GetUser"
 
 	log := c.log.With(slog.String("method", method))
 
 	reqID, err := c.getRequestID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetRequestID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetRequestID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetRequestID, err)
+		return nil, status.Errorf(codes.Internal, "%v: %v", controller.ErrFailedToGetRequestID, err)
 	}
 
 	log = log.With(slog.String("requestID", reqID))
 
 	appID, err := c.getAndValidateAppID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetAndValidateAppID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetAndValidateAppID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetAndValidateAppID, err)
+		// return nil, status.Errorf(codes.Internal, "%v: %v", controller.ErrFailedToGetAndValidateAppID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetAndValidateAppID, err))
 	}
 
 	user, err := c.userUsecase.GetUserByID(ctx, appID)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetUser, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetUser, err)
 		return nil, mapErrorToGRPCStatus(err)
 	}
 
@@ -38,63 +43,63 @@ func (c *controller) GetUser(ctx context.Context, req *ssov1.GetUserRequest) (*s
 }
 
 // TODO: need to return a user data instead of empty struct
-func (c *controller) UpdateUser(ctx context.Context, req *ssov1.UpdateUserRequest) (*ssov1.UpdateUserResponse, error) {
-	const method = "controller.gRPC.UpdateUser"
+func (c *gRPCController) UpdateUser(ctx context.Context, req *ssov1.UpdateUserRequest) (*ssov1.UpdateUserResponse, error) {
+	const method = "сontroller.gRPC.UpdateUser"
 
 	log := c.log.With(slog.String("method", method))
 
 	reqID, err := c.getRequestID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetRequestID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetRequestID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetRequestID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetRequestID, err))
 	}
 
 	log = log.With(slog.String("requestID", reqID))
 
 	if err = validateUpdateUserRequest(req); err != nil {
-		e.HandleError(ctx, log, ErrValidationError, err)
-		return nil, status.Errorf(codes.InvalidArgument, "%v: %v", ErrValidationError, err)
+		e.LogError(ctx, log, controller.ErrValidationError, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrValidationError, err))
 	}
 
 	appID, err := c.getAndValidateAppID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetAndValidateAppID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetAndValidateAppID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetAndValidateAppID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetAndValidateAppID, err))
 	}
 
 	userData := fromUpdateUserRequest(req)
 
 	err = c.userUsecase.UpdateUser(ctx, appID, userData)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToUpdateUser, err)
+		e.LogError(ctx, log, controller.ErrFailedToUpdateUser, err)
 		return nil, mapErrorToGRPCStatus(err)
 	}
 
 	return &ssov1.UpdateUserResponse{}, nil
 }
 
-func (c *controller) DeleteUser(ctx context.Context, req *ssov1.DeleteUserRequest) (*ssov1.DeleteUserResponse, error) {
-	const method = "controller.gRPC.DeleteUser"
+func (c *gRPCController) DeleteUser(ctx context.Context, req *ssov1.DeleteUserRequest) (*ssov1.DeleteUserResponse, error) {
+	const method = "сontroller.gRPC.DeleteUser"
 
 	log := c.log.With(slog.String("method", method))
 
 	reqID, err := c.getRequestID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetRequestID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetRequestID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetRequestID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetRequestID, err))
 	}
 
 	log = log.With(slog.String("requestID", reqID))
 
 	appID, err := c.getAndValidateAppID(ctx)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToGetAndValidateAppID, err)
-		return nil, status.Errorf(codes.Internal, "%v: %v", ErrFailedToGetAndValidateAppID, err)
+		e.LogError(ctx, log, controller.ErrFailedToGetAndValidateAppID, err)
+		return nil, mapErrorToGRPCStatus(fmt.Errorf("%w: %w", controller.ErrFailedToGetAndValidateAppID, err))
 	}
 
 	err = c.userUsecase.DeleteUser(ctx, appID)
 	if err != nil {
-		e.HandleError(ctx, log, ErrFailedToDeleteUser, err)
+		e.LogError(ctx, log, controller.ErrFailedToDeleteUser, err)
 		return nil, mapErrorToGRPCStatus(err)
 	}
 
