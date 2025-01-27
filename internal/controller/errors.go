6e00bc349a4ea632fd69bc33dd595cb282d05ea2
Roman Shelekhov <shelekhoff@gmail.com>
2025-01-28 00:23:44 +0400

    add errors and helper methods

--- internal/controller/errors.go
+++ internal/controller/errors.go
@@ -1,1 +1,27 @@
 package controller
+
+import "errors"
+
+var (
+	ErrValidationError             = errors.New("validation error")
+	ErrRequestIDNotFoundInContext  = errors.New("request ID not found in context")
+	ErrAppIDNotFoundInContext      = errors.New("app ID not found in context")
+	ErrFailedToGetRequestID        = errors.New("failed to get requestID")
+	ErrFailedToGetAppID            = errors.New("failed to get appID")
+	ErrFailedToValidateAppID       = errors.New("failed to validate appID")
+	ErrFailedToGetAndValidateAppID = errors.New("failed to get and validate appID")
+	ErrAppNotFound                 = errors.New("app not found")
+
+	ErrFailedToLoginUser      = errors.New("failed to login user")
+	ErrFailedToRegisterUser   = errors.New("failed to register user")
+	ErrFailedToVerifyEmail    = errors.New("failed to verify email")
+	ErrFailedToResetPassword  = errors.New("failed to reset password")
+	ErrFailedToChangePassword = errors.New("failed to change password")
+	ErrFailedToLogoutUser     = errors.New("failed to logout user")
+	ErrFailedToRefreshTokens  = errors.New("failed to refresh tokens")
+	ErrFailedToGetJWKS        = errors.New("failed to get JWKS")
+
+	ErrFailedToGetUser    = errors.New("failed to get user")
+	ErrFailedToUpdateUser = errors.New("failed to update user")
+	ErrFailedToDeleteUser = errors.New("failed to delete user")
+)
--- internal/controller/grpc/helpers.go
+++ internal/controller/grpc/helpers.go
@@ -1,1 +1,51 @@
 package grpc
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/rshelekhov/sso/internal/controller"
+	"github.com/rshelekhov/sso/internal/domain"
+)
+
+func (c *gRPCController) getRequestID(ctx context.Context) (string, error) {
+	requestID, ok := c.requestIDMgr.FromContext(ctx)
+	if !ok {
+		return "", controller.ErrRequestIDNotFoundInContext
+	}
+
+	return requestID, nil
+}
+
+func (c *gRPCController) getAndValidateAppID(ctx context.Context) (string, error) {
+	appID, err := c.getAppID(ctx)
+	if err != nil {
+		return "", fmt.Errorf("%w: %w", controller.ErrFailedToGetAppID, err)
+	}
+
+	if err = c.validateAppID(ctx, appID); err != nil {
+		return "", fmt.Errorf("%w: %w", controller.ErrFailedToValidateAppID, err)
+	}
+
+	return appID, nil
+}
+
+func (c *gRPCController) getAppID(ctx context.Context) (string, error) {
+	appID, ok := c.appIDMgr.FromContext(ctx)
+	if !ok {
+		return "", controller.ErrAppIDNotFoundInContext
+	}
+
+	return appID, nil
+}
+
+func (c *gRPCController) validateAppID(ctx context.Context, appID string) error {
+	if err := c.appValidator.ValidateAppID(ctx, appID); err != nil {
+		if errors.Is(err, domain.ErrAppNotFound) {
+			return controller.ErrAppNotFound
+		}
+		return err
+	}
+	return nil
+}
