5eb8a05085e5dcc939af709acfb52bc610980072
Roman Shelekhov <shelekhoff@gmail.com>
2025-02-04 00:13:28 +0400

    add method for stopping servers and close db connection

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,10 @@
 package app
 
 import (
+	"fmt"
 	"log/slog"
+
+	"github.com/rshelekhov/sso/internal/infrastructure/storage"
 
 	httpapp "github.com/rshelekhov/sso/internal/app/http"
 
@@ -12,9 +15,29 @@ import (
 type App struct {
 	GRPCServer *grpcapp.App
 	HTTPServer *httpapp.App
+	dbConn     *storage.DBConnection
 }
 
 func New(log *slog.Logger, cfg *config.ServerSettings) (*App, error) {
 	builder := newBuilder(log, cfg)
 	return builder.Build()
+}
+
+func (a *App) Stop() error {
+	const method = "app.Stop"
+
+	// Shutdown gRPC server
+	a.GRPCServer.Stop()
+
+	// Shutdown HTTP server
+	if err := a.HTTPServer.Stop(); err != nil {
+		return fmt.Errorf("%s:failed to stop http server: %w", method, err)
+	}
+
+	// Close database connection
+	if err := a.dbConn.Close(); err != nil {
+		return fmt.Errorf("%s:failed to close database connection: %w", method, err)
+	}
+
+	return nil
 }
