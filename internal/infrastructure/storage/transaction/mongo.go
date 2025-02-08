ed47508929bd23f7a022be481e2085493ac5ffae
Roman Shelekhov <shelekhoff@gmail.com>
2025-02-08 22:24:54 +0400

    update mongo transactions

--- internal/infrastructure/storage/transaction/mongo.go
+++ internal/infrastructure/storage/transaction/mongo.go
@@ -3,15 +3,20 @@ package transaction
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type MongoMgr struct {
-	client *mongo.Client
+	client  *mongo.Client
+	timeout time.Duration
 }
 
 func (tm *MongoMgr) WithinTransaction(ctx context.Context, fn func(ctx context.Context) error) (err error) {
+	ctx, cancel := context.WithTimeout(ctx, tm.timeout)
+	defer cancel()
+
 	session, err := tm.client.StartSession()
 	if err != nil {
 		return fmt.Errorf("failed to start session: %w", err)
@@ -19,8 +24,14 @@ func (tm *MongoMgr) WithinTransaction(ctx context.Context, fn func(ctx context.C
 	defer session.EndSession(ctx)
 
 	_, err = session.WithTransaction(ctx, func(sessCtx mongo.SessionContext) (interface{}, error) {
-		return nil, fn(sessCtx)
+		if err := fn(sessCtx); err != nil {
+			return nil, fmt.Errorf("transaction execution failed: %w", err)
+		}
+		return nil, nil
 	})
+	if err != nil {
+		return fmt.Errorf("tx err: %w", err)
+	}
 
 	return err
 }
