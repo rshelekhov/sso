df6cde7640ae4ff112b5699741f0c39e2b5c20b4
Roman Shelekhov <shelekhoff@gmail.com>
2025-02-09 22:15:27 +0400

    remove struct tags

--- internal/domain/entity/user_device.go
+++ internal/domain/entity/user_device.go
@@ -8,14 +8,14 @@ import (
 
 type (
 	UserDevice struct {
-		ID            string    `db:"id"`
-		UserID        string    `db:"user_id"`
-		AppID         string    `db:"app_id"`
-		UserAgent     string    `db:"user_agent"`
-		IP            string    `db:"ip"`
-		Detached      bool      `db:"detached"`
-		LastVisitedAt time.Time `db:"last_visited_at"`
-		DetachedAt    time.Time `db:"detached_at"`
+		ID            string
+		UserID        string
+		AppID         string
+		UserAgent     string
+		IP            string
+		Detached      bool
+		LastVisitedAt time.Time
+		DetachedAt    time.Time
 	}
 
 	UserDeviceRequestData struct {
