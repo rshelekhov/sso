b82e91e9fdf4c932622971a431047b6caaa1e336
Roman Shelekhov <shelekhoff@gmail.com>
2025-02-25 23:41:16 +0400

    add transaction manager, remove method for deleting tokens, update method for getting user status by id

--- internal/infrastructure/storage/user/postgres/user.go
+++ internal/infrastructure/storage/user/postgres/user.go
@@ -17,11 +17,15 @@ import (
 
 type UserStorage struct {
 	pool    *pgxpool.Pool
-	txMgr   transaction.PostgresManager
+	txMgr   TransactionManager
 	queries *sqlc.Queries
 }
 
-func NewUserStorage(pool *pgxpool.Pool, txMgr transaction.PostgresManager) *UserStorage {
+type TransactionManager interface {
+	ExecWithinTx(ctx context.Context, fn func(tx pgx.Tx) error) error
+}
+
+func NewUserStorage(pool *pgxpool.Pool, txMgr TransactionManager) *UserStorage {
 	return &UserStorage{
 		pool:    pool,
 		txMgr:   txMgr,
@@ -185,10 +189,15 @@ func (s *UserStorage) executeDeleteUser(ctx context.Context, params sqlc.DeleteU
 }
 
 // GetUserStatusByEmail returns the status of the user with the given email
-func (s *UserStorage) GetUserStatusByEmail(ctx context.Context, email string) (string, error) {
+func (s *UserStorage) GetUserStatusByEmail(ctx context.Context, appID, email string) (string, error) {
 	const method = "storage.user.postgres.GetUserStatusByEmail"
 
-	status, err := s.queries.GetUserStatusByEmail(ctx, email)
+	params := sqlc.GetUserStatusByEmailParams{
+		Email: email,
+		AppID: appID,
+	}
+
+	status, err := s.queries.GetUserStatusByEmail(ctx, params)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return "", storage.ErrUserNotFound
@@ -200,10 +209,15 @@ func (s *UserStorage) GetUserStatusByEmail(ctx context.Context, email string) (s
 }
 
 // GetUserStatusByID returns the status of the user with the given userID
-func (s *UserStorage) GetUserStatusByID(ctx context.Context, userID string) (string, error) {
+func (s *UserStorage) GetUserStatusByID(ctx context.Context, appID, userID string) (string, error) {
 	const method = "storage.user.postgres.GetUserStatusByID"
 
-	status, err := s.queries.GetUserStatusByID(ctx, userID)
+	params := sqlc.GetUserStatusByIDParams{
+		ID:    userID,
+		AppID: appID,
+	}
+
+	status, err := s.queries.GetUserStatusByID(ctx, params)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return "", storage.ErrUserNotFound
@@ -212,29 +226,4 @@ func (s *UserStorage) GetUserStatusByID(ctx context.Context, userID string) (str
 	}
 
 	return status, nil
-}
-
-func (s *UserStorage) DeleteAllTokens(ctx context.Context, appID, userID string) error {
-	const method = "storage.user.postgres.DeleteAllTokens"
-
-	params := sqlc.DeleteAllTokensParams{
-		UserID: userID,
-		AppID:  appID,
-	}
-
-	// Delete all tokens within transaction
-	err := s.txMgr.ExecWithinTx(ctx, func(tx pgx.Tx) error {
-		return s.queries.WithTx(tx).DeleteAllTokens(ctx, params)
-	})
-
-	if errors.Is(err, transaction.ErrTransactionNotFoundInCtx) {
-		// Delete all tokens without transaction
-		err = s.queries.DeleteAllTokens(ctx, params)
-	}
-
-	if err != nil {
-		return fmt.Errorf("%s: failed to delete tokens: %w", method, err)
-	}
-
-	return nil
 }
