2f6dc4e4e74611eb7b6047ae55b6b9e82a3ee65d
Roman Shelekhov <shelekhoff@gmail.com>
2025-07-04 23:21:25 +0400

    refactor: migrate storage layer to golib/db
    - Replace custom storage clients with golib/db implementations
    - Update MongoDB connection to use golib/db/mongo
    - Update PostgreSQL connection to use golib/db/postgres/pgxv5
    - Update Redis connection to use golib/db/redis
    - Remove old config conversion functions
    - Simplify storage initialization with context

--- internal/config/settings/cashe.go
+++ internal/config/settings/cashe.go
@@ -2,9 +2,6 @@ package settings
 
 import (
 	"time"
-
-	"github.com/rshelekhov/sso/internal/infrastructure/storage"
-	redisStorage "github.com/rshelekhov/sso/pkg/storage/redis"
 )
 
 type Cache struct {
@@ -20,19 +17,4 @@ type RedisParams struct {
 	MinIdleConns    int           `yaml:"MinIdleConns" default:"5"`
 	SessionTTL      time.Duration `yaml:"SessionTTL" default:"24h"`
 	RevokedTokenTTL time.Duration `yaml:"RevokedTokenTTL" default:"24h"`
-}
-
-func ToRedisConfig(p RedisParams) storage.RedisConfig {
-	return storage.RedisConfig{
-		Redis: &redisStorage.Config{
-			Host:         p.Host,
-			Port:         p.Port,
-			Password:     p.Password,
-			DB:           p.DB,
-			PoolSize:     p.PoolSize,
-			MinIdleConns: p.MinIdleConns,
-		},
-		SessionTTL:      p.SessionTTL,
-		RevokedTokenTTL: p.RevokedTokenTTL,
-	}
 }
--- internal/infrastructure/storage/transaction/postgres.go
+++ internal/infrastructure/storage/transaction/postgres.go
@@ -3,48 +3,27 @@ package transaction
 import (
 	"context"
 	"errors"
-	"fmt"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
+	postgresLib "github.com/rshelekhov/golib/db/postgres/pgxv5"
 )
 
 type PostgresMgr struct {
-	pool *pgxpool.Pool
+	txManager *postgresLib.TransactionManager
 }
-
-type txKey struct{}
 
 var ErrTransactionNotFoundInCtx = errors.New("transaction not found in context")
 
 func (tm *PostgresMgr) WithinTransaction(ctx context.Context, fn func(ctx context.Context) error) (err error) {
-	tx, err := tm.pool.Begin(ctx)
-	if err != nil {
-		return err
-	}
-
-	txCtx := context.WithValue(ctx, txKey{}, tx)
-
-	defer func() {
-		if err != nil {
-			if rbErr := tx.Rollback(ctx); rbErr != nil {
-				err = fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
-			}
-		} else {
-			err = tx.Commit(ctx)
-		}
-	}()
-
-	err = fn(txCtx)
-
-	return err
+	return tm.txManager.RunReadCommitted(ctx, fn)
 }
 
 func (tm *PostgresMgr) ExecWithinTx(ctx context.Context, fn func(tx pgx.Tx) error) error {
-	tx, ok := ctx.Value(txKey{}).(pgx.Tx)
-	if !ok {
-		return ErrTransactionNotFoundInCtx
+	queryEngine := tm.txManager.GetQueryEngine(ctx)
+
+	if tx, ok := queryEngine.(*postgresLib.Transaction); ok {
+		return fn(tx.Tx)
 	}
 
-	return fn(tx)
+	return ErrTransactionNotFoundInCtx
 }
